89b084b8e3e97e965064c6e38022cb1cdec48d40
unknown <rohit22173@gmail.com>
2023-05-07 22:35:54 +0530

    Created a demo for Angular signals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, Signal, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: 'app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular16';
+  movieName = '';
+  moviesList = signal<string[]>([]);
+  moviesCount = computed(()=> this.moviesList().length);
+
+  constructor() {
+    effect(() => console.log('Movie Added:', this.moviesList()));
+  }
+  
+  addMovie() {
+    // We can replacte following with mutate
+    // this.moviesList.mutate(()=> this.moviesList().push(this.movieName));
+    this.moviesList.update(()=> [...this.moviesList(), this.movieName]);
+    this.resetMovieName();
+  }
+
+  clearMovieList() {
+    this.moviesList.update(() => []);
+  }
+
+  resetMovieName() {
+    this.movieName = '';
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,13 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms'
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,9 +18,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    FormsModule,
+    MatButtonModule,
+    MatListModule
   ],
-  providers: [],
+  providers: [provideClientHydration()],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
